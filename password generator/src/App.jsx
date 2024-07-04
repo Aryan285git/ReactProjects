d7620f1915151863ea1149873424b07f98351d3e
Aryan Singh <aryansingh3418@gmail.com>
2024-07-05 02:59:35 +0530

    done

--- password generator/src/App.jsx
+++ password generator/src/App.jsx
@@ -1,33 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCallback, useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [number,setNumber] = useState(false);
+  const [character, setCharacter] = useState(false);
+  const [password, setpassword] = useState("");
 
+  const passwordGenerator = useCallback(() => {
+    let charList = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM";
+    let pass = "";
+    if(number) charList += "0123456789";
+    if(character) charList += "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    for(let i=1; i<=length; i++){
+      let char = Math.floor(Math.random() * charList.length +1);
+      pass += charList.charAt(char)
+    }
+    setpassword(pass);                    
+  }, [length,number,character,setpassword]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [passwordGenerator])
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
+        <h1 className='text-4xl text-center text-white'> Password Generator</h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4 my-4'>
+          <input
+          type="text"
+          value={password}
+          className='outline-none w-full py-1 px-3'
+          placeholder='Password'
+          readOnly
+          />
+          <button className='outline-dashed bg-blue-700 text-white px-3 py-1 shrink-0'>
+            copy
+          </button>
+        </div>
+        <div className='flex text-sm gap-x-2 '>
+          <div className='flex items-center gap-x-2'>
+            <input  
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className='cursor-pointer'
+            onChange={(e) => setLength(e.target.value)}
+            />
+            <label>Length: {length}</label>
+            <div className='flex items-center gap-x-2'>
+              <input
+              type="checkbox"
+              defaultChecked={number}
+              onChange={(e) => setNumber(e.target.checked)}
+              />
+              <label>Numbers</label>
+            </div>
+            <div className='flex items-center gap-x-2'>
+              <input
+              type="checkbox"
+              defaultChecked={character}
+              onChange={(e) => setCharacter(e.target.checked)}
+              />
+              <label>Characters</label>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
